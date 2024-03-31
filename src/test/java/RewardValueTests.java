b36264a485050fb076e0f08b786f19ce9096450b
luisc8487 <lf9904@gmail.com>
2024-03-31 00:51:13 -0400

    fixed task 1 and test file

--- src/test/java/RewardValueTests.java
+++ src/test/java/RewardValueTests.java
@@ -21,17 +21,16 @@ public class RewardValueTests {
     @Test
     void convert_from_cash_to_miles() {
         double cashValue = 100;
-        RewardValue rewardValue = new RewardValue(cashValue);
-        double expectedMilesValue = cashValue * 100;
+        int expectedMilesValue = (int) (cashValue / RewardValue.MILES_TO_CASH_CONVERSION_RATE);
+        var rewardValue = new RewardValue(cashValue);
         assertEquals(expectedMilesValue, rewardValue.getMilesValue());
     }
 
     @Test
     void convert_from_miles_to_cash() {
         int milesValue = 10000;
-        RewardValue rewardValue = new RewardValue(milesValue);
-        double expectedCashValue = milesValue * 0.0035;
-        double tolerance = 0.001;
-        assertEquals(expectedCashValue, rewardValue.getCashValue(), tolerance);
+        double expectedCashValue = milesValue * RewardValue.MILES_TO_CASH_CONVERSION_RATE;
+        var rewardValue = new RewardValue(milesValue);
+        assertEquals(expectedCashValue, rewardValue.getCashValue());
     }
 }
